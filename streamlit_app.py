3b0148fa872a0ca73552e4ec0cddf5983ef69284
fabianbetz <fabian@betzing.de>
2024-11-21 12:55:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,6 +123,8 @@ def is_valid_summary(answers):
         "manually analyze the document",
         "does not contain any text that can be searched",
         "any searchable text",
+        "try uploading it again",
+        "issue retrieving the content",
     ]
     for answer in answers:
         for phrase in invalid_phrases:
